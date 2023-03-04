29e1aa121cc3e0a8ecb66a19647f58eb254eed9f
HammadAli55 <hammadalii@outlook.com>
2023-03-04 17:00:12 +0500

    validator with email

--- app.js
+++ app.js
@@ -1,16 +1,5 @@
-const notes = require('./notes.js')
+const validation = require('validator')
 
-const a = notes("New Note")
+isEmailTrue = validation.isEmail('hammadalii@outlook.com')
 
-// const a = notes(5, 6)
-
-console.log(a)
-
-
-// fs - file system
-// const person = require('./name')
-// const fs = require('fs')
-
-// fs.writeFileSync('notes.txt', 'Whats up')
-
-// fs.appendFileSync
+console.log(isEmailTrue)
